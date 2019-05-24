f287483b9a928b4fa213019b2f1e309962391cab
Robert Burek <robertburek@wp.pl>
2019-05-24 17:26:00 +0200

    Kilka przycisków i OnClick, OnClickListener

--- app/src/main/java/pl/robertburek/nazwaaplikacji/MainActivity.java
+++ app/src/main/java/pl/robertburek/nazwaaplikacji/MainActivity.java
@@ -5,20 +5,38 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mNextButton;
+    private Button PrevButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button_next = findViewById(R.id.button_next);
+        final TextView view1 = findViewById(R.id.textView1);
+        final TextView view2 = findViewById(R.id.textView2);
+        final TextView view3 = findViewById(R.id.textView3);
+        final TextView view4 = findViewById(R.id.textView4);
+        mNextButton = findViewById(R.id.button_next);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Kolejne powitanie zaraz na ekranie", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                //  view1.setVisibility(R.id.textView1);
+            }
+        });
 
+        Button button_prev = findViewById(R.id.button_prev);
+        button_prev.setOnClickListener(new Button_Prev_Class());
 
 
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -54,5 +72,21 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void innyButtonClickHandler(View view) {
+        Snackbar.make(view, "Z metody w pliku XML", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+
+    class Button_Prev_Class implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            Snackbar.make(view, "Poprzednie powitanie zaraz na ekranie", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+            //view.setVisibility(R.id.textView1);
+
+        }
     }
 }
